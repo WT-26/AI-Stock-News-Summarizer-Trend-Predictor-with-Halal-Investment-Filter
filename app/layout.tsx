5c9bb93152fddc1ca1ba53409750abe63d08c6fa
WT-26 <musnowt.wt@gmail.com>
2026-01-15 15:37:40 +0800

    Changing the Title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'AI Financial News Summarizer',
+  description: 'Created with Next.js, and OpenAI API',
+  generator: 'Next.js',
   icons: {
     icon: [
       {
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+  allowedDevOrigins: [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://192.168.56.1",
+    "http://192.168.56.1:3000",
+    "http://192.168.56.0", // optional (safe to include)
+  ],
+};
 
-export default nextConfig
+export default nextConfig;
