c5eee9a42c069bb1274b9114db90fd07c541a229
WT-26 <musnowt.wt@gmail.com>
2026-01-26 22:57:08 +0800

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NewsDashboard } from "@/components/news-dashboard"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-background">
-      <NewsDashboard />
-    </main>
-  )
-}
